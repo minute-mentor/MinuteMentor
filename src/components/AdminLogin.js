913ea372bccd895b39718cd46af36a2e9cb7806f
Baibhav <baibhavk6@gmail.com>
2023-11-19 22:46:12 +0530

    final

--- src/components/AdminLogin.js
+++ src/components/AdminLogin.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Link,useNavigate } from "react-router-dom";
+import backImage from '../../src/images/adminbg.jpeg'
 
 export default function AdminLogin() 
 {
@@ -20,19 +21,32 @@ export default function AdminLogin()
         
       }
 
-  return (
-    <div >
-        <form onSubmit={handleSubmit} class=" p-4 shadow-lg needs-validation mt-5" style={{maxWidth:"50%",margin:"0px auto"}} noValidate>
+  return ( 
+    <div class="ftco-section " style={{backgroundImage:`url(${backImage})`,backgroundRepeat:"no-repeat",paddingTop:"150px",paddingBottom:"350px"}}>
+      <center>
+       <div style={{height:"100%",width:"50%",borderRadius:"40px",marginLeft:"0px",backgroundColor:"rgba(68, 75, 70, 0.56)"}}>
+        <form onSubmit={handleSubmit} class=" py-2  needs-validation " style={{maxWidth:"50%",margin:"0px auto"}} noValidate>
         
         <div class="mb-3">
-             <label for="exampleInputPassword1" class="form-label">Administrator Passcode :</label>
+             <label for="exampleInputPassword1" class="form-label" style={{color:"white",fontSize:"28px"}}>Administrator Passcode :</label>
              <input onChange={handleChangeCode} type="password" class="form-control" id="exampleInputPassword1"/>
         </div>
+
+        <center>
+        <button type="submit" class="btn btn-outline-info">Submit</button>
+
+        </center>
         
-        <button type="submit" class="btn btn-primary">Submit</button>
+        
         
         </form>
       
     </div>
+
+    </center>
+   
+
+    </div>
+    
   )
 }
